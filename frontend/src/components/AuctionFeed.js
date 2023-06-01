a0fb869897dfe2498f25f11b2f156716a6e9ced7
FilipKarlberg <filip.karlberg@hotmail.com>
2023-06-01 17:52:37 +0200

    replace fetch with react-query and axios on auction feed

--- frontend/src/components/AuctionFeed.js
+++ frontend/src/components/AuctionFeed.js
@@ -1,40 +1,24 @@
-import { useEffect } from "react";
-import { useAuctionsContext } from "../hooks/useAuctionsContext";
-import { useAuthContext } from "../hooks/useAuthContext";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 // Components
 import AuctionCard from "../components/AuctionCard";
 
 const AuctionFeed = () => {
-  const { auctions, dispatch } = useAuctionsContext();
-  const { user } = useAuthContext();
+  const { isLoading, data } = useQuery("auction-feed", () => {
+    return axios.get("/api/auctions");
+  });
 
-  useEffect(() => {
-    const fetchAuctions = async () => {
-      const response = await fetch("/api/auctions", {
-        headers: { Authorization: `Bearer ${user.token}` },
-      });
-      const json = await response.json();
-
-      if (response.ok) {
-        // using the dispatch from the useAuctionsContext to update the data locally
-        // (global context instead of local state)
-        dispatch({ type: "SET_AUCTIONS", payload: json });
-      }
-    };
-
-    if (user) {
-      fetchAuctions();
-    }
-  }, [dispatch, user]);
+  if (isLoading) {
+    return <h2>Loading auctions...</h2>;
+  }
 
   return (
     <div className="auction-feed">
       <div className="auctions">
-        {auctions &&
-          auctions.map((auction) => (
-            <AuctionCard key={auction._id} auction={auction} />
-          ))}
+        {data?.data.map((auction) => (
+          <AuctionCard key={auction._id} auction={auction} />
+        ))}
       </div>
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,13 +6,22 @@ import { MessagesContextProvider } from "./context/MessageContext";
 import { AuthContextProvider } from "./context/AuthContext";
 import { AuctionsContextProvider } from "./context/AuctionContext";
 
+// react-query
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <MessagesContextProvider>
         <AuctionsContextProvider>
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
         </AuctionsContextProvider>
       </MessagesContextProvider>
     </AuthContextProvider>
